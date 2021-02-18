5009dbb9248e9d5c42f5c0f960ec996227b2f8b2
asma20 <jbeli.asma20@gmail.com>
2021-02-18 14:13:02 +0100

    addition et multiplication des variables

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercice2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int variable1 = 1;
+            int variable2 = 4;
+            int variable3 = 5;
+            Console.WriteLine("le 1ere variable " + variable1);
+            Console.WriteLine("le 2 éme variable " + variable2);
+            Console.WriteLine("le 3 éme variable " + variable3);
+            int s = variable1 + variable2;
+            Console.WriteLine("la Somme de deux variable " +s);
+            Console.WriteLine("la multiplication avec le 3 éme " + s*variable3);
+
+
+            
+        }
+    }
+}
